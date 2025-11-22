80a15750a06c07369229a790b79063d9f3f82527
bogdansoho <bogdansoho58@gmail.com>
2025-11-22 13:06:37 +0200

    Am creat un cont si m-am delogat

--- src/test/java/SeleniumTests.java
+++ src/test/java/SeleniumTests.java
@@ -15,7 +15,7 @@ public class SeleniumTests {
 
 
     @Test
-    public static void createUser(){
+    public static void createUser() {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://thinking-tester-contact-list.herokuapp.com/");
@@ -25,16 +25,16 @@ public class SeleniumTests {
         signUpButton.click();
 
         WebElement firstNameInput = driver.findElement(By.id("firstName"));
-        firstNameInput.sendKeys("Razvan");
+        firstNameInput.sendKeys("Bogdan");
 
         WebElement lastNameInput = driver.findElement(By.id("lastName"));
-        lastNameInput.sendKeys("Pruteanu");
+        lastNameInput.sendKeys("Soho");
 
         WebElement emailInput = driver.findElement(By.id("email"));
-        emailInput.sendKeys(Utils.generateRandomEmail());
+        emailInput.sendKeys("bogdan1a23@abc.com");
 
         WebElement passwordInput = driver.findElement(By.id("password"));
-        passwordInput.sendKeys("JKDHAKDHiiufd*&*&98_");
+        passwordInput.sendKeys("abc1abc2");
 
         WebElement submitButton = driver.findElement(By.id("submit"));
         submitButton.click();
@@ -42,9 +42,8 @@ public class SeleniumTests {
         WebElement contactListHeaderFound = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[text() = 'Contact List']")));
         Assert.assertTrue(contactListHeaderFound.isDisplayed());
 
-        driver.close();
-
-
+        WebElement logout = driver.findElement(By.id("logout"));
+        logout.click();
 
 
     }
