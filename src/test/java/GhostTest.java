b995467ae9e49f4239d39a8ce7274a4e1c13d4b1
Razvan <ionut.pruteanu@outlook.com>
2025-11-29 05:12:31 +0200

    added ghost and travel

---
+++ src/test/java/GhostTest.java
@@ -0,0 +1,99 @@
+import java.security.Key;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GhostTest {
+
+
+
+    /*
+    Using any language, framework and design pattern please write an
+    automated test that performs the following:
+    A. Navigate to https://ghost.org/
+    B. Navigate to "Start here" section using the "Resources" menu.
+    C. Search for “create new blog”
+    D. Open the 10th result
+    E. Scroll to the top of the page and open the “Pricing” section
+    F. Change the “Based on an audience” slider to 20k members and verify that all the
+    prices have increased.
+    */
+
+    @Test
+    public static void navigateGhost(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://ghost.org/");
+
+        WebElement resourcesButton = driver.findElement(By.xpath("(//button//span[text()='Resources'])"));
+        resourcesButton.click();
+
+        //         List<WebElement> fromOptions = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@data-testid='airport-name']")));
+        // WebElement startHereLink = driver.findElement(By.xpath("//p[contains(text(), 'Start here')]"));
+
+        // WebElement startHereLink = driver.findElement(By.xpath("//p[contains(text(), 'Start here')]")); using wait
+        WebElement startHereLink = new WebDriverWait(driver, java.time.Duration.ofSeconds(20))
+                .until(org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[contains(text(), 'Start here')]")));
+        startHereLink.click();
+
+        WebElement searchBar = new WebDriverWait(driver, java.time.Duration.ofSeconds(20))
+                .until(org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='search-input']")));
+
+        searchBar.sendKeys("create new blog");
+
+        // how to scroll using selenium actions class
+        Actions actions = new Actions(driver);
+        actions.moveToElement(searchBar);
+        actions.click();
+        actions.build().perform();
+
+        WebElement searchResultTen = new WebDriverWait(driver, java.time.Duration.ofSeconds(10))
+                .until(org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='ais-Hits search-results']//li[10]")));
+
+        searchResultTen.click();
+
+        WebElement pricingButton = driver.findElement(By.xpath("//nav//a[contains(@href, 'pricing')]"));
+        pricingButton.click();
+
+        WebElement sliderInput = new WebDriverWait(driver, java.time.Duration.ofSeconds(10))
+                .until(org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='members']")));
+
+        sliderInput.sendKeys(Keys.ARROW_RIGHT);
+
+
+
+        WebElement audienceMembersNo = driver.findElement(By.xpath("//div[@id=\"planselect\"]//span[contains(@class, 'pink')]"));
+        
+        while (!(audienceMembersNo.getText().contains("25,000"))) {
+                sliderInput.sendKeys(Keys.ARROW_RIGHT);
+                System.out.println("Current value: " + audienceMembersNo.getText());
+        }
+
+        WebElement startForFreeLink = driver.findElement(By.xpath("(//a[contains(@href, 'signup') and contains(text(), 'Try for free')])[1]"));
+        Assert.assertFalse(startForFreeLink.isEnabled());
+                
+    }
+
+        // driver.close();
+
+        
+        
+
+
+        // while (!labelText.contains("25K")) {
+        //     sliderInput.sendKeys(Keys.ARROW_RIGHT);
+        //     labelText = sliderLabel.getText();
+        // }
+        
+
+        
+
+}
--- src/test/java/travelProjectTests/TravelProjectTests.java
+++ src/test/java/travelProjectTests/TravelProjectTests.java
@@ -1,90 +1,90 @@
-// package travelProjectTests;
-// import org.openqa.selenium.By;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.WebElement;
-// import org.openqa.selenium.chrome.ChromeDriver;
-// import org.openqa.selenium.support.ui.ExpectedConditions;
-// import org.openqa.selenium.support.ui.WebDriverWait;
-// import org.testng.Assert;
-// import org.testng.annotations.Test;
-// import scoalaInformala.LandingPage;
-// import scoalaInformala.ContactListPage;
-// import utils.Utils;
+package travelProjectTests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import scoalaInformala.LandingPage;
+import scoalaInformala.ContactListPage;
+import utils.Utils;
 
-// import java.time.Duration;
-// import java.util.List;
+import java.time.Duration;
+import java.util.List;
 
-// public class TravelProjectTests {
-
-
-//     @Test
-//     public static void login(){
-//         WebDriver driver = new ChromeDriver();
-//         driver.manage().window().maximize();
-//         driver.get("https://www.easyjet.com/en/");
-
-//         driver.findElement(By.id("ensCloseBanner")).click();
-//         driver.findElement(By.className("TextBox_clearButton__NXFRp")).click();
-//         driver.findElement(By.cssSelector("input[name='from']")).sendKeys("New York");
-
-//         // List<WebElement> fromOptions = driver.findElements(By.xpath("//span[@data-testid='airport-name']"));
-//         List<WebElement> fromOptions = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@data-testid='airport-name']")));
-//         for (WebElement option : fromOptions) {
-//                 // System.out.println(option.getText());
-//                 if(option.getText().contains("Kennedy")) {
-//                     option.click();
-//                     break;
-//                 }
-//         }
-
-//         driver.findElement(By.cssSelector("input[name='to']")).sendKeys("Paris");
-
-//         List<WebElement> toOptions = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@data-testid='airport-name']")));
-//         for (WebElement option : toOptions) {
-//                 // System.out.println(option.getText());
-//                 if(option.getText().contains("Charles de Gaulle")) {
-//                     option.click();
-//                     break;
-//                 }
-//         }
+public class TravelProjectTests {
 
 
-//         WebElement whenElement = driver.findElement(By.id("when"));
-//         whenElement.click();
-//         WebElement dateElement = driver.findElement(By.xpath("//button[@ data-testid='4-12-2025']"));
-//         dateElement.click();
+    @Test
+    public static void login(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.easyjet.com/en/");
 
-//         WebElement who = driver.findElement(By.id("who"));
-//         who.click();
+        driver.findElement(By.id("ensCloseBanner")).click();
+        driver.findElement(By.className("TextBox_clearButton__NXFRp")).click();
+        driver.findElement(By.cssSelector("input[name='from']")).sendKeys("New York");
 
-//         WebElement addAdultButton = driver.findElement(By.xpath("//button[@aria-label='Add one adult']"));
-//         addAdultButton.click();
+        // List<WebElement> fromOptions = driver.findElements(By.xpath("//span[@data-testid='airport-name']"));
+        List<WebElement> fromOptions = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@data-testid='airport-name']")));
+        for (WebElement option : fromOptions) {
+                // System.out.println(option.getText());
+                if(option.getText().contains("Kennedy")) {
+                    option.click();
+                    break;
+                }
+        }
 
-//         WebElement addChildButton = driver.findElement(By.xpath("//button[@aria-label='Add one child']"));
-//         addChildButton.click();
-//         addChildButton.click();
+        driver.findElement(By.cssSelector("input[name='to']")).sendKeys("Paris");
 
-//         WebElement addInfantButton = driver.findElement(By.xpath("//button[@aria-label='Add one infant']"));
-//         addInfantButton.click();
-
-//         // //button[@data-testid="close-button"]
-//         WebElement closeWhoElement = driver.findElement(By.xpath("//button[@data-testid='close-button']"));
-//         closeWhoElement.click();
-
-//         // data-testid="submit"
-//         WebElement submitElement = driver.findElement(By.xpath("//button[@data-testid='submit']"));
-//         submitElement.click();
+        List<WebElement> toOptions = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//span[@data-testid='airport-name']")));
+        for (WebElement option : toOptions) {
+                // System.out.println(option.getText());
+                if(option.getText().contains("Charles de Gaulle")) {
+                    option.click();
+                    break;
+                }
+        }
 
 
+        WebElement whenElement = driver.findElement(By.id("when"));
+        whenElement.click();
+        WebElement dateElement = driver.findElement(By.xpath("//button[@ data-testid='4-12-2025']"));
+        dateElement.click();
+
+        WebElement who = driver.findElement(By.id("who"));
+        who.click();
+
+        WebElement addAdultButton = driver.findElement(By.xpath("//button[@aria-label='Add one adult']"));
+        addAdultButton.click();
+
+        WebElement addChildButton = driver.findElement(By.xpath("//button[@aria-label='Add one child']"));
+        addChildButton.click();
+        addChildButton.click();
+
+        WebElement addInfantButton = driver.findElement(By.xpath("//button[@aria-label='Add one infant']"));
+        addInfantButton.click();
+
+        // //button[@data-testid="close-button"]
+        WebElement closeWhoElement = driver.findElement(By.xpath("//button[@data-testid='close-button']"));
+        closeWhoElement.click();
+
+        // data-testid="submit"
+        WebElement submitElement = driver.findElement(By.xpath("//button[@data-testid='submit']"));
+        submitElement.click();
 
 
 
 
-//     }
+
+
+    }
 
 
         
 
 
 
-// }
+}
