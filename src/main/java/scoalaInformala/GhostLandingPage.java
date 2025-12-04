81b2ffa44dcca00de14283e19967bd263b1aac48
bogdansoho <bogdansoho58@gmail.com>
2025-12-04 12:56:26 +0200

    In clasa GhostLandingPage am initializat elementele cu PageFactory, si adaugat locatiile.

--- src/main/java/scoalaInformala/GhostLandingPage.java
+++ src/main/java/scoalaInformala/GhostLandingPage.java
@@ -1,4 +1,29 @@
 package scoalaInformala;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+
 public class GhostLandingPage {
+    private final WebDriver driver;
+
+    public GhostLandingPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+
+    }
+
+    @FindBy(xpath = "//button[span[text()='Resources']]")
+    public WebElement resourceButton;
+
+    @FindBy(xpath = "//p[contains(text(),'Start here')]")
+    public WebElement startHere;
+
+    @FindBy(xpath = "//input[@id='search-input']")
+    public WebElement searchInput;
+
+    @FindBy(xpath = "//li[@class='ais-Hits-item search-result-item'][10]")
+    public WebElement tenthItem;
+
+    @FindBy(xpath = "//a[text()='Pricing']")
+    public WebElement pricingButton;
 }
