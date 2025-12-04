08cd6ccfc0044123992232f17ee3e93d0bb61bf1
bogdansoho <bogdansoho58@gmail.com>
2025-12-04 12:51:23 +0200

    In clasa PricingPage am initializat WebDriver si Webdriverwait , constructorul , si locatia pentru pagina de preturi.

--- src/main/java/scoalaInformala/PricingPage.java
+++ src/main/java/scoalaInformala/PricingPage.java
@@ -1,4 +1,21 @@
 package scoalaInformala;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.*;
+
 public class PricingPage {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public PricingPage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//div[@class='relative max-w-8xl mx-auto pb-[12vmin] pt-[8vmin] text-center']")
+    public WebElement pricingHeader;
 }
