193649f102880f7e94087c6eac319b388c21967f
bogdansoho <bogdansoho58@gmail.com>
2025-12-04 12:49:46 +0200

    In clasa StartHere am initializat WebDriver si Webdriverwait , constructorul , si locatia pentru prima pagina.

--- src/main/java/scoalaInformala/StartHerePage.java
+++ src/main/java/scoalaInformala/StartHerePage.java
@@ -1,4 +1,23 @@
 package scoalaInformala;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.*;
+
 public class StartHerePage {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public StartHerePage(WebDriver driver){
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//div[@class='flex justify-start']")
+    public WebElement startHereHeader;
 }
+
