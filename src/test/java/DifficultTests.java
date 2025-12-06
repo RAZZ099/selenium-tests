3dc3d83037d0b8bda946cf0803a38eec0493995f
RAZZ099 <ionut.pruteanu@outlook.com>
2025-12-06 11:13:24 +0200

    added priceCheck for publisherPrice before and after the slider

--- src/test/java/DifficultTests.java
+++ src/test/java/DifficultTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -50,6 +51,12 @@ public class DifficultTests {
 
         pricingButton.click();
 
+        WebElement publisherPriceInitial = driver.findElement(By.xpath("//p[@data-price='publisher']"));
+
+        String publisherPriceTextInitial = publisherPriceInitial.getText();
+
+        Assert.assertEquals(publisherPriceTextInitial, "29");
+
         WebElement sliderInput = driver.findElement(By.xpath("//input[@id='members']"));
 
         WebElement audienceMembersNo = driver.findElement(By.xpath("//span[contains(@class, 'js-tooltip-count')]"));
@@ -57,6 +64,19 @@ public class DifficultTests {
         while (!(audienceMembersNo.getText().contains("25k"))) {
             sliderInput.sendKeys(Keys.ARROW_RIGHT);
         }
+
+
+        By publisherPriceBy = By.xpath("//p[@data-price='publisher']");
+
+        WebElement publisherPriceUpdated = driver.findElement(By.xpath("//p[@data-price='publisher']"));
+
+        WebDriverWait customWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        customWait.until(ExpectedConditions.textToBePresentInElementLocated(publisherPriceBy, "141"));
+
+        String publisherPriceTextUpdated = publisherPriceUpdated.getText();
+
+        Assert.assertEquals(publisherPriceTextUpdated, "141");
 
 
 
