a283aa016746826fc4a05efc8ff90b9805bcdc44
burlacugeorge <burlacugeorge22@gmail.com>
2025-12-06 11:43:35 +0200

    Am terminat testul din clasa DifficultTests.java

--- src/test/java/DifficultTests.java
+++ src/test/java/DifficultTests.java
@@ -1,10 +1,12 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -48,6 +50,45 @@ public class DifficultTests {
         actions.moveToElement(pricingButton).perform();
 
         pricingButton.click();
+
+        WebElement publisherPriceInitial = driver.findElement(By.xpath("//p[@data-price='publisher']"));
+        String publisherPriceTextInitial = publisherPriceInitial.getText();
+        Assert.assertEquals(publisherPriceTextInitial, "29");
+
+
+        WebElement sliderInput = driver.findElement(By.xpath("//input[@id='members']"));
+
+        WebElement audienceMembersNo = driver.findElement(By.xpath("//span[contains(@class, 'js-tooltip-count')]"));
+
+
+        while (!(audienceMembersNo.getText().contains("25k"))) {
+            sliderInput.sendKeys(Keys.ARROW_RIGHT);
+        }
+
+        By publisherPriceBy = By.xpath("//p[@data-price='publisher']");
+
+        WebElement publisherPriceUpdated = driver.findElement(By.xpath("//p[@data-price='publisher']"));
+
+        WebDriverWait customWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        customWait.until(ExpectedConditions.textToBePresentInElementLocated(publisherPriceBy, "141"));
+
+        String publisherPriceTextUpdated = publisherPriceUpdated.getText();
+
+        Assert.assertEquals(publisherPriceTextUpdated, "141");
+
+
+        By businessPriceBy = By.xpath("//p[@data-price='business']");
+
+        WebElement businessPriceUpdated = driver.findElement(By.xpath("//p[@data-price='business']"));
+
+        WebDriverWait customsWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        customsWait.until(ExpectedConditions.textToBePresentInElementLocated(businessPriceBy, "266"));
+
+        String businessPriceTextUpdated = businessPriceUpdated.getText();
+
+        Assert.assertEquals(businessPriceTextUpdated, "266");
 
 
     }
